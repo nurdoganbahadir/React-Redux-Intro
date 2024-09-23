d53d56f9a50c1c5f11c15304d2d10e5be37841dd
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-23 22:33:11 +0300

    şarta bağlı stiller verildi

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -2,30 +2,22 @@ import React from "react";
 import okLogo from "../../assets/ok.png";
 import deleteLogo from "../../assets/delete.png";
 
-
-const TodoItem = ({gorev}) => {
-
-
+const TodoItem = ({ gorev }) => {
   return (
     <div
-    
+      style={{
+        textDecoration: gorev.completed ? "line-through" : "none",
+        backgroundColor: gorev.completed ? "lightgray" : "orange",
+      }}
       className="todo-list"
     >
       <h2 className="todoText">{gorev.yazi}</h2>
       <div>
         <span>
-          <img
-            src={okLogo}
-            className="ok-logo"
-            alt="ok logo"
-          />
+          <img src={okLogo} className="ok-logo" alt="ok logo" />
         </span>
         <span>
-          <img
-            src={deleteLogo}
-            className="delete-logo"
-            alt="delete logo"
-          />
+          <img src={deleteLogo} className="delete-logo" alt="delete logo" />
         </span>
       </div>
     </div>
