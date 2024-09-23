127c2af8e17dbb67bdcd045e2bb4d108b364f159
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-23 21:40:34 +0300

    arttir tamamladnı

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./Counter.css";
+import { useDispatch, useSelector } from "react-redux";
 import { arttir } from "../../redux/actions/counterActions";
 
 const Counter = () => {
+  const { sayac, text } = useSelector((state) => state.counterReducer);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h2 className="counter-header" onClick={arttir}>
-        Counter with Redux
-      </h2>
-      <h1>0</h1>
-      <h2>text</h2>
+      <h2 className="counter-header">Counter with Redux</h2>
+      <h1>{sayac}</h1>
+      <h2>{text}</h2>
       <div>
-        <button className="counter-button positive">ARTTIR</button>
+        <button
+          onClick={() => dispatch(arttir())}
+          className="counter-button positive"
+        >
+          ARTTIR
+        </button>
         <button className="counter-button">RESET</button>
         <button className="counter-button negative">AZALT</button>
       </div>
--- src/redux/reducers/counterReducer.jsx
+++ src/redux/reducers/counterReducer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const counterReducer = (state= { sayac: 0, text: "" },action) => {
-  return <div>counterReducer</div>;
+const counterReducer = (state = { sayac: 0, text: "" }, { type, payload }) => {
+  switch (type) {
+    case "ARTTIR":
+      return { sayac: state.sayac + 1, text: payload };
+    default:
+      return state;
+  }
 };
 
 export default counterReducer;
