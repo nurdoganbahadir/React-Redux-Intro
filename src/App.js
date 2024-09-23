1a01612726750092c5735ca2172524cf339a613b
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-23 22:00:32 +0300

    todo başlandı app'e eklendi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Provider } from "react-redux";
 import "./App.css";
 import Counter from "./components/counter/Counter";
 import { store } from "./redux/store";
+import Todo from "./components/todo/Todo";
 
 const App = () => {
   return (
     <div className="app">
       <Provider store={store}>
         <Counter />
+        <Todo />
       </Provider>
     </div>
   );
